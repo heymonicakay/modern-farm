1e9dd06fac7a07e73a8e12e4d1eba2339794fb39
heymonicakay <hey.monicakay@gmail.com>
2020-07-22 15:24:08 -0500

    harvest??

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,3 +1,24 @@
 // n this module, define and export a harvestPlants function.
 
 // The harvestPlants function must accept the plants array as input.
+export const harvestPlants = ( plantsArray ) => {
+      // The function will return an array of seed objects.
+      // - need empty array and return statement. 
+      // Iterate the array of growing plants. 
+      plantsArray.forEach( plantObject => {
+
+            let seedArray = []
+            // On each plant, get the value of the output property.
+            if (plantObject.type === "Corn") {
+                 seedArray.push(plantObject * (plantObject.output/2))
+            }
+            else {
+                  seedArray.push(plantObject * plantObject.output)
+            }
+      })
+      return seedArray
+}
+
+// Add that many of the plant objects to the array that the function returns. For example, if the current plant is a peanut object with an output of 2...
+
+// Again, the exception is corn. Half of your corn will be sold to cattle ranchers, so only half of the output of each corn plant will be added to the array that this function returns.
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ console.log("Welcome to the main module")
 import { createPlan } from "./plan.js";
 import { plantSeeds } from "./tractor.js";
 import { usePlants } from "./field.js";
-
+import { harvestPlants } from "./harvester"
 
 const yearlyPlan = createPlan()
 // In the main.js module, invoke the function that plants all of the seeds, 
@@ -14,3 +14,7 @@ plantSeeds(yearlyPlan)
 const allCrops = usePlants()
 
 console.log(allCrops)
+
+const cropYeild = harvestPlants()
+
+console.log(cropYeild)
